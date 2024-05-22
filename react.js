2e3282b6f51c74f0c6983b374053178a416bb977
Bolbi85 <93646286+Bolbi85@users.noreply.github.com>
2024-05-22 19:33:15 +0200

    Create react.js

---
+++ react.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, wereld!</h1>
+      <p>Dit is een voorbeeld van een React.js-bestand gegenereerd door ChatGPT.</p>
+    </div>
+  );
+}
+
+export default App;
